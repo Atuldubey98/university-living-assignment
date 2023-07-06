702f7909b3f844dde14d0890012b14ed7c51f02a
Atuldubey98 <atuldubey017@gmail.com>
2023-07-06 10:57:23 +0530

    Feature : Calender spirals added

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -11,6 +11,13 @@ export default function Calender(props: CalenderProps) {
   const { calender } = props;
   return (
     <div className="calender">
+      <div className="calender__spiral"></div>
+      <div className="calender__spiral"></div>
+      <div className="calender__spiral"></div>
+      <div className="calender__spiral"></div>
+      <div className="calender__spiral"></div>
+      <div className="calender__spiral"></div>
+
       <h1>{calender.date}</h1>
       <div className="calender__footer">
         {calender.month} / {calender.year}
